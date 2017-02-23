afc16b7467703f5797d191f83e6137da772eff1d
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-02-22 19:37:32 -0600

    adding meats

--- meats.js
+++ meats.js
@@ -1,14 +1,22 @@
 // This SandwichMaker IIFE augments the original one
-var SandwichMaker = (function(maker) {
+var SandwichMaker = (function(meatsMaker) {
 
   // Private variable to store the different meat prices
-  var meatPrices;
+  // var meatPrices;
 
   // Augment the original object with another method
-  maker.addMeat = function() {
-    return ???;
+  maker.addMeat = function(meats) {
+    var meatPrices = {
+      Turkey: 1.90,
+      Bacon: 1.85,
+      Chicken: 1.25,
+      Ham: 1.35
+    }
+    return meatPrices
+    var userMeatSelect = options[meats];
+    console.log("Meats", userMeatSelect);
   };
 
   // Return the new, augmented object with the new method on it
-  return maker;
+  return meatsMaker;
 })(SandwichMaker);
---
+++ veggies.js
@@ -0,0 +1,22 @@
+// This SandwichMaker IIFE augments the original one
+var SandwichMaker = (function(veggiesMaker) {
+
+  // Private variable to store the different meat prices
+  // var meatPrices;
+
+  // Augment the original object with another method
+  maker.addVeggie = function(veggies) {
+    var veggiePrices = {
+      Lettuce: 0.30,
+      Tomato: 0.20,
+      Onion: 0.25,
+      Pickles: 0.75
+    }
+    return veggiePrices
+    var userVeggieSelect = options[veggies];
+    console.log("Veggies", userVeggieSelect);
+  };
+
+  // Return the new, augmented object with the new method on it
+  return veggiesMaker;
+})(SandwichMaker);
