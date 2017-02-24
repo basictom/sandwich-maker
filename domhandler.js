0d3c38a741da10e51c9449c03ce3e587ec33f335
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-02-24 17:12:52 -0600

    stuck on event listners

--- domhandler.js
+++ domhandler.js
@@ -1,21 +1,26 @@
-// Variable to hold the final price. Default to 0.
-var finalSandwichPrice = 0;
-
 var breadSelect = document.getElementById("bread-container");
 var meatSelect = document.getElementById("meat-container");
 var cheeseSelect = document.getElementById("cheese-container");
 var condimentSelect = document.getElementById("condiments-containers");
 var veggieSelect = document.getElementById("veggies-contianer");
+var output = document.getElementById("sandwich");
 
+// Variable to hold the final price. Default to 0.
 
-
+var finalSandwichPrice = 0;
 
 
 // Variable to hold topping that the user selects
-var selectedTopping;
+var selectedTopping = document.getElementById("");
 
 // Get a reference to the <select> element that has all the meat options
-var meatChooser = document.getElementById("meat-chooser");
+var meatChooser = document.getElementById("meat-container");
+
+// meatChooser.addEventListener("change", function(event){
+//   var userMeat = event.target.value;
+//   SandwichMaker.addMeat(userMeat);
+//   console.log(userMeat);
+// });
 
 /*
   A <select> element broadcasts a change event, so you listen for it
@@ -23,7 +28,10 @@ var meatChooser = document.getElementById("meat-chooser");
 */
 meatChooser.addEventListener("change", function(event) {
   // Get the value chosen from the DOM
-  selectedTopping = event.target.value;
+  var userMeat = event.target.value;
+  // selectedTopping = event.target.value;
+  SandwichMaker.addMeat(userMeat);
+  console.log(userMeat);
 
   // Determine the price of the topping chosen
 
